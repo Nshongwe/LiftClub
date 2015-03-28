c056d610ade02d855aec4fdc3560125816300cef
Nshongwe <nshongwe@gmail.com>
2015-03-28 13:48:07 +0200

    MianProject

---
+++ LiftClub/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using LiftClub.Resources;
+
+namespace LiftClub
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
