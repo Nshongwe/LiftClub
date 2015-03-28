a3b7fadfed296375cc6f47b2834569ed58515dfa
Treezo <treezomsk786@gmail.com>
2015-03-28 17:09:48 +0200

    gui

---
+++ LiftClub/BackEnd/SubscriberDetails.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace LiftClub.BackEnd
+{
+    public partial class SubscriberDetails : UserControl
+    {
+        public SubscriberDetails()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_TextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+
+        }
+    }
+}
